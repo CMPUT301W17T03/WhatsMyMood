7c12d03a1f39b9c80f627799b2d4170ce26a6db8
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-02-24 00:19:22 -0700

    Added javadocs

--- WhatsMyMood/app/src/main/java/com/example/whatsmymood/MoodList.java
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/MoodList.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 /**
  * Created by Malcolm on 2017-02-23.
  */
+
 //TODO add observable implementation
 public class MoodList {
     private ArrayList<Mood> moods;
@@ -16,6 +17,12 @@ public class MoodList {
     public void addMood(Mood mood){
         this.moods.add(mood);
     }
+
+    /**
+     * Adds and Removes moods from the list
+     * @param index
+     * @param mood
+     */
     public void addMood(int index, Mood mood){
         this.moods.add(index, mood);
     }
@@ -26,31 +33,57 @@ public class MoodList {
         this.moods.remove(index);
     }
 
+    /**
+     * Get's index of the mood
+     * @param mood
+     * @return
+     */
     public int getIndex(Mood mood){
         for (int i = 0; i < this.getSize(); i++){
             if(moods.get(i) == mood){return i;}
         }
         return -1;
-        //TODO search for mood
     }
+
+    /**
+     * Get's mood from index
+     * @param index
+     * @return
+     */
     public Mood get(int index){
        return this.moods.get(index);
     }
 
+    /**
+     * Gets the size of the moodList
+     * @return
+     */
     public int getSize(){
         return this.moods.size();
     }
 
+    /**
+     * Get's the most recent mood
+     * @return
+     */
     public Mood getRecentMood(){
         //TODO search earliest date
         return null;
     }
 
+    /**
+     * Gets the sorted mood list
+     * @return
+     */
     public ArrayList<Mood> getSortedMoodList(){
         //TODO sort the moods by date
         return this.moods;
     }
 
+    /**
+     * Gets the mood list
+     * @return
+     */
     public ArrayList<Mood> getMoodList(){
         return this.moods;
     }
