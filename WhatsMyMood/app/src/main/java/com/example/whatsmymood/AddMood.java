dc9b59b16c1babebe3e393f06ced452b715c7e80
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-03-07 20:54:12 -0700

    Added popup screen

--- WhatsMyMood/app/src/main/java/com/example/whatsmymood/AddMood.java
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/AddMood.java
@@ -1,8 +1,24 @@
 package com.example.whatsmymood;
 
+import android.widget.EditText;
+
 /**
  * Created by nathan on 07/03/17.
  */
 
 public class AddMood {
+    private EditText moodType;
+
+    // Lazy Singleton
+    private static AddMood instance = null;
+
+    private AddMood() {}
+
+    public static AddMood getInstance() {
+        if (instance == null) {
+            instance = new AddMood();
+        }
+        return instance;
+    }
+
 }
