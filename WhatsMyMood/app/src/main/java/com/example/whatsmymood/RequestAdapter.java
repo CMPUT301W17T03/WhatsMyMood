8281864d60ef709693656717866038fe0f3fe6d1
EJTang <ejtang@ualberta.ca>
2017-03-07 17:49:45 -0700

    Create RequestAdapter

---
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/RequestAdapter.java
@@ -0,0 +1,17 @@
+package com.example.whatsmymood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The type Request adapter.
+ * Created By ejtang 07/03/2017
+ */
+public class RequestAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_adapter);
+    }
+}
