e3445fba0acee0aa1459a555c03099a2e1e98a9a
Cirieneve <yiji1@ualberta.ca>
2017-03-11 13:41:27 -0700

    Started ElasticSearch controllers.

---
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/ElasticSearchMoodController.java
@@ -0,0 +1,34 @@
+package com.example.whatsmymood;
+
+import android.os.AsyncTask;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+/**
+ * Created by YiJi on 2017-03-11.
+ */
+
+public class ElasticSearchMoodController {
+
+    private static JestDroidClient client;
+
+    public static class AddMoodsTask extends AsyncTask<Mood, Void, Void> {
+
+    }
+
+
+    //Taken from LonelyTwitter - Lab 5 with ElasticSearch
+    public static void verifySettings() {
+        if (client == null) {
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
+
+}
---
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/ElasticSearchUserController.java
@@ -0,0 +1,31 @@
+package com.example.whatsmymood;
+
+import android.os.AsyncTask;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+
+/**
+ * Created by YiJi on 2017-03-11.
+ */
+
+public class ElasticSearchUserController {
+
+    private static JestDroidClient client;
+
+
+    //Taken from LonelyTwitter - Lab 5 with ElasticSearch
+    public static void verifySettings() {
+        if (client == null) {
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
+
+}
