97a3da0e548b18fa9f0a52420ddea53ab2bcb590
AlexCzeto <alexczeto@yahoo.ca>
2017-03-11 17:05:00 -0700

    Basic create user implemented

--- WhatsMyMood/app/src/main/java/com/example/whatsmymood/CreateUserController.java
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/CreateUserController.java
@@ -7,8 +7,17 @@ package com.example.whatsmymood;
 public class CreateUserController {
 
     public CreateUserController(){
+    }
 
+    public void create(String username, String password){
 
+        CurrentUser current = CurrentUser.getInstance();
 
+        UserAccount user = new UserAccount(username,password);
+
+        current.setCurrentUser(user);
+
+        // TODO add to Elastic Search
+        // TODO check that username is unique using Elastic Search
     }
 }
--- WhatsMyMood/app/src/main/java/com/example/whatsmymood/UserCreateActivity.java
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/UserCreateActivity.java
@@ -2,12 +2,40 @@ package com.example.whatsmymood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UserCreateActivity extends AppCompatActivity {
+
+    private EditText userName ;
+    private EditText password ;
+    private EditText passwordConfirm ;
+    private Button createAccount;
+    private Button cancelButton;
+    private CreateUserController userController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_create);
+
+        this.userName = (EditText) findViewById(R.id.username);
+        this.password = (EditText) findViewById(R.id.password1);
+        this.passwordConfirm = (EditText) findViewById(R.id.password2);
+        this.createAccount = (Button) findViewById(R.id.create);
+        this.cancelButton = (Button) findViewById(R.id.cancel);
+
+        this.userController = new CreateUserController();
     }
+
+    public void clickCreate(){
+        if ( password.getText().toString().equals(passwordConfirm.getText().toString())){
+            userController.create(userName.getText().toString(),password.getText().toString());
+        }
+    }
+
+    public void clickCancel(){
+
+    }
+
 }
