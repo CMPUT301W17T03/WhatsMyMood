f008c0948523a458ca3829752ac79da95035cf0c
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-03-13 13:15:59 -0600

    Fix Dummies

--- WhatsMyMood/app/src/main/java/com/example/whatsmymood/MainActivity.java
+++ WhatsMyMood/app/src/main/java/com/example/whatsmymood/MainActivity.java
@@ -30,18 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /**
-         * USE THIS TO MANUALLY ADD A FOLLOWER
-         * STEPS:
-         * 1. TYPE THE USER YOU WANT TO FOLLOW
-         * 2. LAUNCH THE APP AND LOGIN
-         * 3. MAKE A SINGULAR MOOD
-         * 4. LOG OUT
-         * 5. COMMENT
-         */
-        //user = current.getCurrentUser();
-        //user.getFollows().addToFollowing("John");
-
         setContentView(R.layout.activity_main);
         LinearLayout footer = (LinearLayout)findViewById(R.id.footer);
         FooterHandler handler = new FooterHandler(this, footer);
